3acf0d9d887ed250fcf2221d576eaa3c8c03fc9f
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-03-22 17:31:30 +0430

    Fill galaxy table with 6 rows

--- Project1-CelestialBodiesDatabase/universe.sql
+++ Project1-CelestialBodiesDatabase/universe.sql
@@ -11,3 +11,12 @@ CREATE TABLE galaxy (
 	is_ring_galaxy BOOLEAN NOT NULL,
 	description TEXT NOT NULL
 );
+
+-- Fill galaxy table (ChatGPT is used for getting the information)
+INSERT INTO galaxy (name, age_million_years, diameter_thousand_light_years, solar_mass, has_active_galactic_nucleus, is_ring_galaxy, description) VALUES
+('Milky Way', 13000, 100, 1, FALSE, FALSE, 'The galaxy that contains our Solar System.'),
+('Andromeda', 6000, 220, 1.2, TRUE, FALSE, 'The closest spiral galaxy to the Milky Way.'),
+('Whirlpool', 300, 60, 0.3, FALSE, TRUE, 'A classic example of a grand-design spiral galaxy.'),
+('Centaurus A', 12000, 12, 1.2, TRUE, FALSE, 'A peculiar galaxy that appears to be the result of a merger.'),
+('NGC 4414', 2000, 80, 0.5, FALSE, FALSE, 'A beautiful barred spiral galaxy located in the constellation Coma Berenices.'),
+('M82', 10, 5, 0.01, TRUE, FALSE, 'A starburst galaxy with an intense burst of star formation occurring in its nucleus.');
