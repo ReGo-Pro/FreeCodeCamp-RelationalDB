42df93927c056e31794e14b4a075f4b00ecfc69d
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-03-22 17:57:57 +0430

    Create star table

--- Project1-CelestialBodiesDatabase/universe.sql
+++ Project1-CelestialBodiesDatabase/universe.sql
@@ -20,3 +20,15 @@ INSERT INTO galaxy (name, age_million_years, diameter_thousand_light_years, sola
 ('Centaurus A', 12000, 12, 1.2, TRUE, FALSE, 'A peculiar galaxy that appears to be the result of a merger.'),
 ('NGC 4414', 2000, 80, 0.5, FALSE, FALSE, 'A beautiful barred spiral galaxy located in the constellation Coma Berenices.'),
 ('M82', 10, 5, 0.01, TRUE, FALSE, 'A starburst galaxy with an intense burst of star formation occurring in its nucleus.');
+
+-- Create star table
+CREATE TABLE star (
+	star_id SERIAL PRIMARY KEY, 
+	name VARCHAR(255) UNIQUE NOT null,
+	age_million_years INT NOT NULL,
+	diameter_thousand_kilometers INT NOT NULL,
+	solar_mass DECIMAL NOT NULL,
+	is_visible BOOLEAN NOT NULL,
+	is_exoplanet_host BOOLEAN NOT NULL,
+	description TEXT NOT NULL
+);
