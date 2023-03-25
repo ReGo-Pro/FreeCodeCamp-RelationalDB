b810536dddefb109863b96b771385f0e86494526
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-03-25 23:31:56 +0330

    Modify the script to add teams and games

--- Project2-WorldCupDatabase/insert_data.sh
+++ Project2-WorldCupDatabase/insert_data.sh
@@ -13,20 +13,18 @@ function INSERT_TEAM() {
   if [[ -z $TEAM_ID ]]
   then
     INSERT_RESULT=$($PSQL "INSERT INTO teams (name) VALUES ('$1')")
-    if [[ $INSERT_RESULT == "INSERT 0 1" ]]
-    then
-      echo "Succeessfully inserted team: $1"
-    else 
-      echo "Some error occured please make sure database is connected and try again"
-    fi
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$1'")
   fi
+
+  echo $TEAM_ID
 }
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER_NAME OPPONENT_NAME WINNER_GOALS OPPONENT_GOALS
 do 
   if [[ $YEAR != "year" ]] # skip first line
-  then 
-    INSERT_TEAM "$WINNER_NAME"
-    INSERT_TEAM "$OPPONENT_NAME"
+  then
+    WINNER_ID=$(INSERT_TEAM "$WINNER_NAME")
+    OPPONENT_ID=$(INSERT_TEAM "$OPPONENT_NAME")
+    INSERT_RESULT=$($PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
 done
