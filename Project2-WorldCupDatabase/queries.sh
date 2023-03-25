7d98c1a85561dca93328f28e38992f7a4b777a99
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-03-26 00:45:34 +0330

    Query for getting total number of goals in all games from winning teams

---
+++ Project2-WorldCupDatabase/queries.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo -e "\nTotal number of goals in all games from winning teams:"
+echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
