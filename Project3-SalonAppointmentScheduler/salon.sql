d1bcd7d6e4cb5478f33e7175548fa0497e5a68ed
Reza Ghochkhani <43405506+ReGo-Pro@users.noreply.github.com>
2023-03-26 01:20:53 +0330

    Create appointments table

--- Project3-SalonAppointmentScheduler/salon.sql
+++ Project3-SalonAppointmentScheduler/salon.sql
@@ -5,3 +5,12 @@ create table customers (
 	name VARCHAR(255) NOT NULL,
 	phone VARCHAR(255) UNIQUE NOT NULL
 );
+
+create table appointments (
+	appointment_id SERIAL PRIMARY KEY,
+	customer_id INT NOT NULL,
+	service_id INt NOT NULL,
+	time VARCHAR(255),
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+	FOREIGN KEY (service_id) REFERENCES services(service_id)
+);
