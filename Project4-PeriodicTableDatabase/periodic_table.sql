d18baafc61971efb3e1b9cf15b267d97ebaae27f
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-03-31 19:37:57 +0330

    melting_point_celsius and boiling_point_celsius columns should not accept null values

--- Project4-PeriodicTableDatabase/periodic_table.sql
+++ Project4-PeriodicTableDatabase/periodic_table.sql
@@ -2,3 +2,5 @@ ALTER TABLE properties RENAME COLUMN weight TO atomic_mass;
 ALTER TABLE properties RENAME COLUMN melting_point TO melting_point_celsius;
 ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;
 
+ALTER TABLE properties ALTER COLUMN melting_point_celsius NOT NULL;
+ALTER TABLE properties ALTER COLUMN boiling_point_celsius NOT NULL;
