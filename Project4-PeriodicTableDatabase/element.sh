b060a988d3800cc72a5be429c6ef46cf90cf1b4f
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-04-01 14:44:04 +0330

    Get element if valid argument is provided

---
+++ Project4-PeriodicTableDatabase/element.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+function GET_ELEMENT() {
+  if [[ "$1" =~ ^[0-9]+$ ]]
+  then 
+    ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number=$1")
+  else 
+    ELEMENT=$($PSQL "SELECT * FROM elements WHERE name='$1' OR symbol='$1'")
+  fi
+
+  echo $ELEMENT
+}
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else 
+  GET_ELEMENT $1
+fi
