418b396e89f3bd07bea4c4a809ea7c0d3e9338dc
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-04-01 13:42:49 +0330

    Add not-null type_id foreign key to properties table, update data and remove redundant data

--- Project4-PeriodicTableDatabase/periodic_table.sql
+++ Project4-PeriodicTableDatabase/periodic_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE types (
 	type_id SERIAL PRIMARY KEY,
-	type VARCHAR(50) NOT NULL
+	type VARCHAR(16) NOT NULL
 );
 
 insert into types (type) values ('metal'), ('nonmetal'), ('metalloid');
@@ -18,3 +18,13 @@ ALTER TABLE elements ADD CONSTRAINT unique_element_name UNIQUE (name);
 ALTER TABLE elements ADD CONSTRAINT unique_element_symbol UNIQUE (symbol);
 
 ALTER TABLE properties ADD CONSTRAINT fk_properties_elements FOREIGN KEY (atomic_number) REFERENCES elements(atomic_number);
+
+-- Add forign key constraint to properties table, update valus, drop unnecessary column
+ALTER TABLE properties ADD type_id INT;
+ALTER TABLE properties ADD CONSTRAINT fk_properties_types FOREIGN KEY (type_id) REFERENCES types(type_id);
+UPDATE properties SET type_id = (SELECT type_id FROM types WHERE type = 'metal') WHERE type = 'metal';
+UPDATE properties SET type_id = (SELECT type_id FROM types WHERE type = 'nonmetal') WHERE type = 'nonmetal';
+UPDATE properties SET type_id = (SELECT type_id FROM types WHERE type = 'metalloid') WHERE type = 'metalloid';
+ALTER TABLE properties ALTER COLUMN type_id SET NOT NULL;
+ALTER TABLE properties DROP COLUMN type;
+
