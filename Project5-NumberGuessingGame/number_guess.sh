59d60aa89e41d61815f71e17ac780f792db3199e
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-04-02 21:09:19 +0330

    Implement game logic - dummy

--- Project5-NumberGuessingGame/number_guess.sh
+++ Project5-NumberGuessingGame/number_guess.sh
@@ -23,18 +23,35 @@ function HANDLE_USER() {
 
 # Pass generated_number as argument
 function HANDLE_GUESSED_NUMBER() {
-  echo $1
+  GENERATED_NUMBER=$1
   while [[ -z $GUESSED_NUMBER ]]
   do
     read GUESSED_NUMBER
     if [[ "$GUESSED_NUMBER" =~ ^[0-9]+$ ]]
     then 
-      echo "Yeah, number"
+      ### INNER if
+      if [[ $GUESSED_NUMBER -lt $GENERATED_NUMBER ]]
+      then 
+        echo "It's higher than that, guess again:"
+        unset GUESSED_NUMBER
+      elif [[ $GUESSED_NUMBER -gt $GENERATED_NUMBER ]]
+      then
+        echo "It's lower than that, guess again:" 
+        unset GUESSED_NUMBER
+      else 
+        echo "You guessed it in <number_of_guesses> tries. The secret number was <secret_number>. Nice job!"
+      fi
+      ### END INNER if
     else 
       echo "That is not an integer, guess again:"
       unset GUESSED_NUMBER
     fi
   done
+}
+
+# $1: guessed number - $2 generated number
+function GAME_LOGIC() {
+  echo "$1 -- $2" 
 }
 
 ### main
