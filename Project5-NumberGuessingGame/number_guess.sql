f49acb4670b21a24d7afe6dc318cf47a62fcbead
Reza Ghochkhani <r.ghochkhani@gmail.com>
2023-04-02 19:50:34 +0330

    Make two additional user columns not-nullable

--- Project5-NumberGuessingGame/number_guess.sql
+++ Project5-NumberGuessingGame/number_guess.sql
@@ -7,3 +7,6 @@ CREATE TABLE users (
 
 ALTER TABLE users ADD games_played INT;
 ALTER TABLE users ADD best_game INT;
+
+ALTER TABLE users ALTER COLUMN games_played SET NOT NULL;
+ALTER TABLE users ALTER COLUMN best_game SET NOT NULL;
